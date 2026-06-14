68bc2824ba3dffafde67c496b74e495403ec1239
Rishab-del <rishabh24464@iiitd.ac.in>
2026-06-14 23:04:48 +0530

    fix duplicate express declaration

--- backend/Server.js
+++ backend/Server.js
@@ -187,7 +187,6 @@ const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 require("dotenv").config();
 
-const express = require("express");
 const app = express();
 const http = require("http");
 const mongoose = require("mongoose");
