a721984d263173ffbb9d0eb34fad2b573862f586
Rishab-del <rishabh24464@iiitd.ac.in>
2026-06-14 22:49:08 +0530

    fix render port

--- backend/Server.js
+++ backend/Server.js
@@ -1084,9 +1084,8 @@ app.use("/api/user", router);
    SERVER START
 ========================= */
 
-server.listen(5001, () => {
+const PORT = process.env.PORT || 5001;
 
-  console.log(
-    "Server running on port 5001"
-  );
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
