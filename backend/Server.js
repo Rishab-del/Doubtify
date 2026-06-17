77c2c29477b70b7c145f5e0ec2fe4e3e3c28cae5
Rishab-del <rishabh24464@iiitd.ac.in>
2026-06-18 01:25:02 +0530

    fix socket cors

--- backend/Server.js
+++ backend/Server.js
@@ -91,8 +91,13 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://doubtify-git-main-rishabh-team.vercel.app",
+      "https://doubtify-five.vercel.app",
+    ],
     methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
