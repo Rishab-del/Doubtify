8df763bfea29be5ab72a78ea1c78962f4b9f0cb6
Rishab-del <rishabh24464@iiitd.ac.in>
2026-06-19 17:33:25 +0530

    add navbar

--- src/components/Home/Offcanvas/Calendar.js
+++ src/components/Home/Offcanvas/Calendar.js
@@ -1,25 +1,34 @@
 import React, { useState } from "react";
 import ReactCalendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+
 import "./Calendar.css";
+
+import Navbar from "../Navbar";
+import BackButton from "./BackButton";
 
 export default function Calendar() {
   const [date, setDate] = useState(new Date());
 
   return (
-    <div className="calendar-page">
-      <div className="calendar-card">
-        <h1>📅 Study Calendar</h1>
+    <>
+      <Navbar />
+      <BackButton />
 
-        <ReactCalendar
-          onChange={setDate}
-          value={date}
-        />
+      <div className="calendar-page">
+        <div className="calendar-card">
+          <h1>📅 Study Calendar</h1>
 
-        <div className="selected-date">
-          Selected Date: {date.toDateString()}
+          <ReactCalendar
+            onChange={setDate}
+            value={date}
+          />
+
+          <div className="selected-date">
+            Selected Date: {date.toDateString()}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
