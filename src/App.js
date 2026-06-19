6938aed320ea62df775680c3e46788d7b7d2699f
Rishab-del <rishabh24464@iiitd.ac.in>
2026-06-19 16:45:33 +0530

    change in calender

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ import AIOptions from "./components/Dashboard/AIOptions";
 import AIchat from "./components/Dashboard/AIchat";
 import Discussion from "./components/Dashboard/Discussion";
 import Signup from "./components/Login/signup";
-import CalendarPage from "./components/Home/Offcanvas/Calendar";
+import Calendar from "./components/Home/Offcanvas/Calendar";
 
 import PrivateRoute from "./components/PrivateRoute";
 
@@ -122,8 +122,7 @@ function Layout() {
             </PrivateRoute>
           }
         />
-        <Route path="/calendar" element={<Calendar />} />
-
+        <Route path="/calendar" element={<PrivateRoute> <Calendar /> </PrivateRoute>} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
